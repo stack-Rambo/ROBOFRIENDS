da699b02331e319fb52da61593b6ed064c36ce12
Stack-Rambo <me.rehanshaikh01@gmail.com>
2020-03-07 15:07:40 +0530

    added new files to the project

---
+++ build/precache-manifest.e17f255f6e11154092bc8f3efe981183.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15bc16fa40712f3364c19961f8535d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbe208f9e9255d0e4589",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d60558244e00a38c15b8",
+    "url": "/static/css/main.36fe2b73.chunk.css"
+  },
+  {
+    "revision": "fbe208f9e9255d0e4589",
+    "url": "/static/js/2.8ee06294.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.8ee06294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d60558244e00a38c15b8",
+    "url": "/static/js/main.62a7eabe.chunk.js"
+  },
+  {
+    "revision": "640d9de3af6513b25eff",
+    "url": "/static/js/runtime-main.d89e57b9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
