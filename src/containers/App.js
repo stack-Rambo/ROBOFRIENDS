d3028cd6346f598434bb775f13f791d5165e7112
Stack-Rambo <me.rehanshaikh01@gmail.com>
2020-03-07 14:48:32 +0530

    new Features added

---
+++ src/containers/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import CardList from "../component/CardList";
+import SearchBox from "../component/SearchBox";
+import Scroll from "../component/Scroll";
+
+import "./App.css";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: ""
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(user => this.setState({ robots: user }));
+  }
+
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />;
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
